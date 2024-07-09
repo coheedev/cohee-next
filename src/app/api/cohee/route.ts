8234da98315f5d5154f757f61f484c3be0a6cf36
ysung327 <ysung327@naver.com>
2024-07-09 20:56:49 +0900

    ADD: cohee api route

--- src/app/api/cohee/route.ts
+++ src/app/api/cohee/route.ts
@@ -1,7 +1,10 @@
-// import neo4jDriver from "@/utils/Neo4j";
-import { openai } from "@ai-sdk/openai";
+// 여기는 API 라우터입니다.
+// /api/cohee 경로로 들어오는 요청을 처리합니다.
+// vercel에서 제공하는 AI SDK를 사용하여 OpenAI 호출을 하고 있습니다.
+// 아래 링크의 docs를 참고하여 작성해주세요.
 // https://sdk.vercel.ai/docs/getting-started/nextjs-app-router
 
+import { openai } from "@ai-sdk/openai";
 import { StreamingTextResponse, streamText, StreamData } from "ai";
 
 // Allow streaming responses up to 30 seconds
